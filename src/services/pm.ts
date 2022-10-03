172d52e9495aef200b0449f6330444486194be15
Ida Bagus Ketut Adiyoga <adiyoga27new@gmail.com>
2022-10-04 00:04:40 +0800

    Update PM2

--- src/services/pm.ts
+++ src/services/pm.ts
@@ -5,7 +5,10 @@ import * as dotenv from "dotenv";
 const process = require('process');
 require('dotenv').config()
 
-export const restartApp = function (message: any) {
+export const restartApp = function () {
+    console.log("\x1b[33m%s\x1b[0m", "Function Restart APP Running");
+    console.log("\x1b[33m%s\x1b[0m", process.env.APP_NAME);
+
     pm2.connect(function (err) {
         if (err) {
             console.log(err)
